15f21690a0806f626170f3c6380c5b3a94b14505
KHUSHI AGGARWAL <khushiaggarwal311@gmail.com>
2026-04-24 20:19:21 +0530

    Delete Self Learning/tuple_string_dictionary.py directory

--- Self Learning/tuple_string_dictionary.py/converttupletolist.py
+++
@@ -1,9 +0,0 @@
-#convert tuple to a list and modifie it
-t = (10, 20, 30)
-lst = list(t)
-lst.append(40)
-t = tuple(lst)
-print("Modified Tuple:", t)
-
-#output
-"""Modified Tuple: (10, 20, 30, 40)"""
